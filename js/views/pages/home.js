dd40eeb90ba219a2c046485ce403658981db501c
nasjcodes <5616230+nasjcodes@users.noreply.github.com>
2019-01-31 18:16:54 +0800

    Added contact icons (all icons links to github for now)

--- js/views/pages/home.js
+++ js/views/pages/home.js
@@ -1,6 +1,11 @@
 const headerHtml = /* html */`
-  <h1>Hi, I'm Jian Sheng.</h1>
-  <p>Aspiring Software Developer.</p>
+  <h1 class="head-lg">Hi, I'm Jian Sheng.</h1>
+  <p class="text-lg">Aspiring Software Developer.</p>
+  <div>
+    <a href="https://github.com/nasjcodes" class="link-inherit"><i class="fas fa-envelope-square fa-3x"></i></a>
+    <a href="https://github.com/nasjcodes" class="link-inherit"><i class="fab fa-github-square fa-3x"></i></a>
+    <a href="https://github.com/nasjcodes" class="link-inherit"><i class="fab fa-linkedin fa-3x"></i></a>
+  </div>
 `;
 
 const contentHtml = /* html */`
