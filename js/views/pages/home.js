6b7b01f6934e16c3514d1eccb1ebba54b5f5c445
nasjcodes <5616230+nasjcodes@users.noreply.github.com>
2019-02-11 14:01:39 +0800

    Added labels for home page icons

--- js/views/pages/home.js
+++ js/views/pages/home.js
@@ -2,9 +2,17 @@ const headerHtml = /* html */`
   <h1 class="head-lg">Hi, I'm Jian&nbspSheng.</h1>
   <p class="text-lg">Aspiring Software Developer.</p>
   <div>
-    <a href="https://github.com/nasjcodes" class="link-inherit"><i class="fas fa-envelope-square fa-3x"></i></a>
-    <a href="https://github.com/nasjcodes" class="link-inherit"><i class="fab fa-github-square fa-3x"></i></a>
-    <a href="https://github.com/nasjcodes" class="link-inherit"><i class="fab fa-linkedin fa-3x"></i></a>
+    <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="Email">
+      <i class="fas fa-envelope-square fa-3x" aria-hidden="true" title="Email"></i>
+    </a>
+
+    <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="GitHub">
+      <i class="fab fa-github-square fa-3x" aria-hidden="true" title="Github"></i>
+    </a>
+
+    <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="LinkedIn">
+      <i class="fab fa-linkedin fa-3x" aria-hidden="true" title="LinkedIn"></i>
+    </a>
   </div>
 `;
 
