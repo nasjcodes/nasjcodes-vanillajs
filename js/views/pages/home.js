bd35e263293ec917ac5ee77dbb8806c118a5af91
nasjcodes <5616230+nasjcodes@users.noreply.github.com>
2019-02-19 20:50:35 +0800

    Updated home page

--- js/views/pages/home.js
+++ js/views/pages/home.js
@@ -4,25 +4,19 @@ const headerHtml = /* html */`
 `;
 
 const contentHtml = /* html */`
-  <div>
-    <a href="mailto:nasjcodes@outlook.com" class="link-inherit" aria-label="Email">
-      <i class="fas fa-envelope-square fa-3x icon-row" aria-hidden="true" title="Email"></i>
+  <div class="flex-container">
+    <a href="/res/Resume_Ang-Jian-Sheng.pdf" target="_blank">
+      <div class="button">
+        <span class="link-inherit">Résumé</span>
+      </div>
     </a>
 
-    <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="GitHub" target="_blank">
-      <i class="fab fa-github-square fa-3x icon-row" aria-hidden="true" title="Github"></i>
-    </a>
-
-    <a href="https://www.linkedin.com/in/jiansheng/" class="link-inherit" aria-label="LinkedIn" target="_blank">
-      <i class="fab fa-linkedin fa-3x icon-row" aria-hidden="true" title="LinkedIn"></i>
+    <a href="/contact" onclick="route('/contact'); return false;">
+        <div class="button">
+          <span class="link-inherit">Contact</span>
+        </div>
     </a>
   </div>
-
-  <a href="/res/Resume_Ang-Jian-Sheng.pdf" target="_blank">
-    <div class="button">
-      <span class="link-inherit">Résumé</span>
-    </div>
-  </a>
 `;
 
 const home = {
