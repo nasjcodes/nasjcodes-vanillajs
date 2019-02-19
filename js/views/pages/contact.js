07d8d21e7081e4819608f2e23d4cb55e370fd4e7
nasjcodes <5616230+nasjcodes@users.noreply.github.com>
2019-02-19 20:56:05 +0800

    Switched position of linkedin and github buttons on contact page

--- js/views/pages/contact.js
+++ js/views/pages/contact.js
@@ -6,22 +6,6 @@ const contentHtml = /* html */`
   <article>
     <p>Feel free to contact me through the following links!</p>
 
-    <!--
-    <div>
-      <a href="mailto:nasjcodes@outlook.com" class="link-inherit" aria-label="Email">
-        <i class="fas fa-envelope-square fa-3x icon-row" aria-hidden="true" title="Email"></i>
-      </a>
-
-      <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="GitHub" target="_blank">
-        <i class="fab fa-github-square fa-3x icon-row" aria-hidden="true" title="Github"></i>
-      </a>
-
-      <a href="https://www.linkedin.com/in/jiansheng/" class="link-inherit" aria-label="LinkedIn" target="_blank">
-        <i class="fab fa-linkedin fa-3x icon-row" aria-hidden="true" title="LinkedIn"></i>
-      </a>
-    </div>
-    -->
-
     <div class="flex-container">
       <a href="mailto:nasjcodes@outlook.com">
         <div class="button">
@@ -30,17 +14,17 @@ const contentHtml = /* html */`
         </div>
       </a>
 
-      <a href="https://github.com/nasjcodes" target="_blank">
-        <div class="button">
-          <i class="fab fa-github"></i>
-          <span class="link-inherit">GitHub</span>
-        </div>
-      </a>
-
       <a href="https://www.linkedin.com/in/jiansheng/" target="_blank">
         <div class="button">
           <i class="fab fa-linkedin-in"></i>
           <span class="link-inherit">LinkedIn</span>
+        </div>
+      </a>
+
+      <a href="https://github.com/nasjcodes" target="_blank">
+        <div class="button">
+          <i class="fab fa-github"></i>
+          <span class="link-inherit">GitHub</span>
         </div>
       </a>
 
