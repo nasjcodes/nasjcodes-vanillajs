e99e9d9cee4a3732776252185d727e280fbe8e51
nasjcodes <5616230+nasjcodes@users.noreply.github.com>
2019-02-19 16:32:36 +0800

    Home page links now open in a new tab

--- js/views/pages/home.js
+++ js/views/pages/home.js
@@ -2,15 +2,15 @@ const headerHtml = /* html */`
   <h1 class="head-lg">Hi, I'm Jian&nbspSheng.</h1>
   <p class="text-lg">Aspiring Software Developer.</p>
   <div>
-    <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="Email">
+    <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="Email" target="_blank">
       <i class="fas fa-envelope-square fa-3x" aria-hidden="true" title="Email"></i>
     </a>
 
-    <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="GitHub">
+    <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="GitHub" target="_blank">
       <i class="fab fa-github-square fa-3x" aria-hidden="true" title="Github"></i>
     </a>
 
-    <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="LinkedIn">
+    <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="LinkedIn" target="_blank">
       <i class="fab fa-linkedin fa-3x" aria-hidden="true" title="LinkedIn"></i>
     </a>
   </div>
