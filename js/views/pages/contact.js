f1dffb7f74a16081f71bd71f28d95e8c625548ce
nasjcodes <5616230+nasjcodes@users.noreply.github.com>
2019-02-19 20:47:17 +0800

    Updated contact page

--- js/views/pages/contact.js
+++ js/views/pages/contact.js
@@ -1,27 +1,57 @@
 const headerHtml = /* html */`
-  <h1>Contact Me</h1>
+  <h1>Get in touch.</h1>
 `;
 
 const contentHtml = /* html */`
-  <div>
-    <a href="mailto:nasjcodes@outlook.com" class="link-inherit" aria-label="Email">
-      <i class="fas fa-envelope-square fa-3x icon-row" aria-hidden="true" title="Email"></i>
-    </a>
+  <article>
+    <p>Feel free to contact me through the following links!</p>
 
-    <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="GitHub" target="_blank">
-      <i class="fab fa-github-square fa-3x icon-row" aria-hidden="true" title="Github"></i>
-    </a>
+    <!--
+    <div>
+      <a href="mailto:nasjcodes@outlook.com" class="link-inherit" aria-label="Email">
+        <i class="fas fa-envelope-square fa-3x icon-row" aria-hidden="true" title="Email"></i>
+      </a>
 
-    <a href="https://www.linkedin.com/in/jiansheng/" class="link-inherit" aria-label="LinkedIn" target="_blank">
-      <i class="fab fa-linkedin fa-3x icon-row" aria-hidden="true" title="LinkedIn"></i>
-    </a>
-  </div>
+      <a href="https://github.com/nasjcodes" class="link-inherit" aria-label="GitHub" target="_blank">
+        <i class="fab fa-github-square fa-3x icon-row" aria-hidden="true" title="Github"></i>
+      </a>
 
-  <a href="/res/Resume_Ang-Jian-Sheng.pdf" target="_blank">
-    <div class="button">
-      <span class="link-inherit">Résumé</span>
+      <a href="https://www.linkedin.com/in/jiansheng/" class="link-inherit" aria-label="LinkedIn" target="_blank">
+        <i class="fab fa-linkedin fa-3x icon-row" aria-hidden="true" title="LinkedIn"></i>
+      </a>
     </div>
-  </a>
+    -->
+
+    <div class="flex-container">
+      <a href="mailto:nasjcodes@outlook.com">
+        <div class="button">
+          <i class="fas fa-envelope"></i>
+          <span class="link-inherit">E-mail</span>
+        </div>
+      </a>
+
+      <a href="https://github.com/nasjcodes" target="_blank">
+        <div class="button">
+          <i class="fab fa-github"></i>
+          <span class="link-inherit">GitHub</span>
+        </div>
+      </a>
+
+      <a href="https://www.linkedin.com/in/jiansheng/" target="_blank">
+        <div class="button">
+          <i class="fab fa-linkedin-in"></i>
+          <span class="link-inherit">LinkedIn</span>
+        </div>
+      </a>
+
+      <a href="/res/Resume_Ang-Jian-Sheng.pdf" target="_blank">
+        <div class="button">
+          <i class="fas fa-file"></i>
+          <span class="link-inherit">Résumé</span>
+        </div>
+      </a>
+    </div>
+  </article>
 `;
 
 const contact = {
@@ -29,7 +59,7 @@ const contact = {
   header: headerHtml,
   content: contentHtml,
   centerX: true,
-  centerY: true,
+  centerY: false,
 };
 
 export default contact;
