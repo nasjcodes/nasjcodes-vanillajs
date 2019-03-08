04b82650c0b868d9ec8f6e7bdc66fb6680c4c473
nasjcodes <5616230+nasjcodes@users.noreply.github.com>
2019-03-08 18:16:11 +0800

    Fixed bug where redirect.js redirects to the main site

--- js/lib/redirect.js
+++ js/lib/redirect.js
@@ -1,4 +1,4 @@
 // Avoid loading one extra file to get the base path
 const base = '/nasjcodes-vanillajs';
 const url = window.location.pathname.slice(base.length + 1) || 'home';
-window.location = `/?redirect=${url}`;
+window.location = `${base}/?redirect=${url}`;
